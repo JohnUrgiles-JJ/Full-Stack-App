4919051a791547dcda988e3d42d30cd956c7b164
John Urgiles <john.ju226@gmail.com>
2025-12-01 15:14:04 -0500

    fourth

--- server/index.js
+++ server/index.js
@@ -4,20 +4,17 @@ console.log('DB_PASSWORD:', process.env.DB_PASSWORD)
 const express = require('express')
 const app = express()
 const db = require('./queries') // we can now reference objects in our queries file
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 // Middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-// CREATE
-app.post('/links', db.createLinks)
-// READ
-app.get('/links', db.getLinks)
-// UPDATE 
-app.put('/links/:id', db.updateLinks)
-// DELETE
-app.delete('/links/:id', db.deleteLinks)
+// CRUD METHODS
+app.post('/api/links', db.createLinks)
+app.get('/api/links', db.getLinks)
+app.put('/api/links/:id', db.updateLinks)
+app.delete('/api/links/:id', db.deleteLinks)
 
 // host react app as static files
 app.use(express.static(path.resolve(__dirname, '../client/dist')))
@@ -27,9 +24,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'))
 })
 
+module.exports = app
 
-
-
-app.listen(PORT, () => {
-    console.log(`The app is running on port ${PORT}.`)
-})
+if (require.main === module) {
+    app.listen(PORT, () => {
+        console.log(`The app is running on port ${PORT}.`)
+    })
+}
